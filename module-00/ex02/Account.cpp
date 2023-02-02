dcfd43f74493c2f32204a6587ce1ea9768db65f0
root <root@DESKTOP-9QJ1LHQ>
2023-02-02 12:41:14 +0000

    casa

--- module-00/ex02/Account.cpp
+++ module-00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 22:05:16 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/02/01 22:24:24 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/02/02 12:39:44 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,25 @@
 #include <iostream>
 
 Account::Account(int initial_deposit) {
+	Account::_displayTimestamp();
+	std::cout << Account::getNbAccounts();
 	this->_amount = initial_deposit;
-	this->_totalAmount = initial_deposit;
-	this->_nbDeposits++;
+	Account::_totalAmount += initial_deposit;
+	Account::_nbAccounts++;
 	return;
 }
 
 Account::~Account(void) {
 	return;
 }
+
+int	Account::_totalAmount = 0;
+
+int	Account::_nbAccounts = 0;
+
+int	Account::_totalNbDeposits = 0;
+
+int	Account::_totalNbWithdrawals = 0;
 
 int	Account::getNbAccounts(void) {
 	return (Account::_nbAccounts);
@@ -44,11 +54,24 @@ int	Account::getNbAccounts(void) {
 	return (Account::_nbAccounts);
 }
 
+void Account::makeDeposit(int deposit)
+{
+	if (deposit <= 0)
+		return;
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	this->_nbDeposits++;
+	return;
+}
+
 // void Account::_displayTimestamp(void) {
 // 	std::cout <<
 // }
 
-// void Account::displayAccountsInfos(void) {
-// 	std::cout << Account::_displayTimestamp();
-	
-// }
+void Account::displayAccountsInfos(void) {
+	Account::_displayTimestamp();
+	Account::getNbAccounts();
+	Account::getTotalAmount();
+	Account::getNbDeposits;
+	Account::getNbWithdrawals();
+}
