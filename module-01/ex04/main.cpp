4ac92a03efa609600a4fc6c716cbea3cabaf0cb9
Lucas filipe Martins da silva <lucas-ma@c2r7s5.42lisboa.com>
2023-02-06 14:16:38 +0000

    a

--- module-01/ex04/main.cpp
+++ module-01/ex04/main.cpp
@@ -6,15 +6,36 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 17:35:20 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/02/06 12:10:20 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/02/06 14:10:32 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostre
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <string.h>
 #include <stdlib.h>
+
+int create_replacing_file(std::ifstream my_file, int ac, char **av)
+{
+	std::string str1;
+	std::string str2;
+	std::string name_rp_file;
+	std::ofstream replace_file;
+	size_t		pos_of_dot;
+
+	str1 = av[2];
+	str2 = av[3];
+	pos_of_dot = name_rp_file.rfind(".");
+	name_rp_file = name_rp_file.substr(0, pos_of_dot);
+	name_rp_file += std::string("replace");
+	(void)my_file;
+	(void)ac;
+	std::cout << name_rp_file << std::endl;
+	return (1);
+}
 
 int main(int ac, char **av) {
 
@@ -29,6 +50,7 @@ int main(int ac, char **av) {
 	
 	my_file.open(av[1]);
 	if (my_file.is_open())
+		// std::cout << "file opened!" << std::endl;
 		create_replacing_file(my_file, ac, av);
 	else
 	{
