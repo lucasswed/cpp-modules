e44e3dd4082980a81092ed2b7143de2a1cc37cc0
Lucas filipe Martins da silva <lucas-ma@c2r7s6.42lisboa.com>
2023-02-07 14:11:36 +0000

    pc

--- module-01/ex06/main.cpp
+++ module-01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 17:06:49 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/02/07 14:06:26 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/02/07 14:11:09 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,19 @@ int	main(int ac, char **av) {
 		std::cout << "Correct usage: ./harlFilter <one of the four levels>" << std::endl;
 		return (EXIT_FAILURE);
 	}
+	Harl
+	std::string level(av[1]);
+	int	i;
 	
+	i = 0;
+	switch (level.compare())
+	{
+		case /* constant-expression */:
+			/* code */
+			break;
+		
+		default:
+			break;
+	}
 	return 0;
 }
