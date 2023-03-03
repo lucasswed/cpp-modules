ee707a7a0a24f00442aad464275b052d387e5629
lucasSilva <lucas.silva@live.com.pt>
2023-03-03 18:16:03 +0000

    done module04

---
+++ module-04/ex03/src/MateriaSource.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 18:05:49 by lucas-ma          #+#    #+#             */
+/*   Updated: 2023/03/03 18:15:25 by lucas-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource(void) : IMateriaSource() {
+	for (size_t i = 0; i < 4; i++)
+		this->learned_materias = NULL;
+}
+
+MateriaSource::~MateriaSource {
+}
+
+void	MateriaSource::learnMateria(AMateria *m) {
+	for (size_t i = 0; i < 4; i++)
+		if (this->learned_materias[i] == NULL)
+		{
+			this->learned_materias[i] = m;
+			return ;
+		}
+}
+
+AMateria*	MateriaSource::createMateria(std::string const& type) {
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (this->learned_materias[i] && this->learned_materias[i]->getType() == type)
+			return (this->learned_materias[i].clone());
+	}
+}
--- module-04/ex03/src/MateriaSource.hpp
+++ module-04/ex03/src/MateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 14:57:55 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/03/03 14:59:48 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:04:09 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 # define MATERIASOURCE_HPP
 # include "IMateriaSource.hpp"
 
-class MateriaSource : public IMATERIASOURCE_HPP
+class MateriaSource : public IMateriaSource
 {
 	private:
-		
+		AMateria *learned_materias[4];
 	public:
 		MateriaSource(void);
 		~MateriaSource(void);
+
+		virtual void	learnMateria(AMateria *m);
+		virtual AMateria*	createMateria(std::string const& type);
 };
 
 #endif
