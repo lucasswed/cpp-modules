63ce5b75e8bd86d292cf529fecf4aed77badc414
Lucas filipe Martins da silva <lucas-ma@c2r7s6.42lisboa.com>
2023-03-07 16:56:27 +0000

    more tests add to ex00 module 05

--- module-05/ex00/src/main.cpp
+++ module-05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 16:14:28 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/03/07 16:42:05 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:55:40 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,20 @@ int main(void) {
 		Bureaucrat dario("dario", 1);
 		Bureaucrat rafa("rafa", 150);
 		Bureaucrat pedro("pedro", 5);
-		Bureaucrat ricardo("ricardo", -1);
+		Bureaucrat ricardo("ricardo", 25);
 
-		std::cout << dario << rafa << pedro << ricardo;
+		std::cout << dario << std::endl;
+		std::cout << rafa << std::endl;
+		std::cout << pedro << std::endl;
+		std::cout << ricardo << std::endl;
+		// rafa.decrement_grade();
+		// dario.increment_grade();
+		pedro.increment_grade();
+		ricardo.decrement_grade();
+		std::cout << std::endl << dario << std::endl;
+		std::cout << rafa << std::endl;
+		std::cout << pedro << std::endl;
+		std::cout << ricardo << std::endl;
 	}
 	catch(std::exception& e)
 	{
