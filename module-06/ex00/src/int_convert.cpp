c544362aa7fe87c84412fb5550d1bd89b4d55b98
Lucas filipe Martins da silva <lucas-ma@c2r7s6.42lisboa.com>
2023-03-28 15:03:34 +0100

    a

--- module-06/ex00/src/int_convert.cpp
+++ module-06/ex00/src/int_convert.cpp
@@ -6,12 +6,19 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 12:45:02 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/03/28 14:11:14 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:03:18 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "convertions.hpp"
 
 void	int_convert(std::string const& input) {
-	
+	std::stringstream midterm;
+	long int	num;
+
+	midterm << input;
+	midterm >> num;
+	std::cout << "char: ";
+	if (is)
+	std::cout << 
 }
