f8ee9cbda15c0fd671daa251303005327e702570
lucasSilva <lucas.silva@live.com.pt>
2023-05-10 14:38:54 +0100

    nice

--- module-08/ex02/src/MutantStack.hpp
+++ module-08/ex02/src/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:11:52 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/05/04 16:30:31 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:31:53 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ class MutantStack : public std::stack<T, Container>
 		MutantStack&	operator=(MutantStack const& rhs);
 
 		// Definition of the container iterator just to iterator
-		typedef typename	T::template Container<T>::iterator				iterator;
-		typedef typename	T::template Container<T>::reverse_iterator		reverse_iterator;
+		typedef typename	MutantStack<T, Container>::container_type::iterator				iterator;
+		typedef typename	MutantStack<T, Container>::container_type::reverse_iterator		reverse_iterator;
 
 		// Iterator functions
 		iterator			begin(void);
@@ -57,22 +57,22 @@ MutantStack<T, Container>&	MutantStack<T, Container>::operator=(MutantStack cons
 
 template <typename T, typename Container>
 typename MutantStack<T, Container>::iterator		MutantStack<T, Container>::begin(void) {
-	return (Container<T>::begin());
+	return (this->std::stack<T, Container>::c.begin());
 }
 
 template <typename T, typename Container>
 typename MutantStack<T, Container>::iterator		MutantStack<T, Container>::end(void) {
-	return (Container<T>::end());
+	return (this->std::stack<T, Container>::c.end());
 }
 
 template <typename T, typename Container>
-typename MutantStack<T, Container>::iterator		MutantStack<T, Container>::rbegin(void) {
-	return (std::reverse_iterator(Container<T>::begin()));
+typename MutantStack<T, Container>::reverse_iterator		MutantStack<T, Container>::rbegin(void) {
+	return (this->std::stack<T, Container>::c.rbegin());
 }
 
 template <typename T, typename Container>
-typename MutantStack<T, Container>::iterator		MutantStack<T, Container>::rend(void) {
-	return (std::reverse_iterator(Container<T>::end()));
+typename MutantStack<T, Container>::reverse_iterator		MutantStack<T, Container>::rend(void) {
+	return (this->std::stack<T, Container>::c.rend());
 }
 
 #endif
