85689382cc149e67ee20a9749d246e58a0ad7bd2
Lucas filipe Martins da silva <lucas-ma@c1r20s2.42lisboa.com>
2023-05-29 15:33:39 +0100

    to home

--- module-09/ex00/src/main.cpp
+++ module-09/ex00/src/main.cpp
@@ -6,30 +6,32 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 10:36:38 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/05/22 14:16:15 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/05/29 15:30:04 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(int ac, char** av) {
-	if (ac != 2) {
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
 		std::cout << "Invalid arguments number" << std::endl;
 		return (1);
 	}
 
 	std::ifstream data(av[1]);
-	if (!data.is_open()) {
+	if (!data.is_open())
+	{
 		std::cout << "Can't open the " << av[1] << "file" << std::endl;
 		return (1);
 	}
 
-	std::map<std::string, float>	container;
+	std::map<std::string, float> container;
 	fillMapContainer(data, container);
 	for (std::map<std::string, float>::iterator i = container.begin(); i != container.end(); i++)
 	{
-		std::cout << i << std::endl;
+		std::cout << "key: " << i->first << " value: " << i->second << std::endl;
 	}
-
 	return (0);
 }
